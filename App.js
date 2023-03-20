ca4bc1f6d510bf2a177aa0245e2d28486f830dc3
andre-vg <avictorg@gmail.com>
2023-03-20 16:25:39 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: '#fff'}}>V0 do PCD-in</Text>
+      <StatusBar style="inverted" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#fff',
+  },
+});
